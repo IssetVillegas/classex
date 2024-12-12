583e9ae2b17c06e92af3c81049e2a4c573dcfd30
IssetVillegas <2022189@student.cct.ie>
2024-12-12 13:05:51 +0000

    Update myex.py

--- myex.py
+++ myex.py
@@ -3,8 +3,33 @@ import streamlit as st
 st.title("My New App")
 st.write("This is my new Streamlit app Isset Villegas")
 
+st.write("This is a change")
+
 row1 = st.columns(3)
 row2 = st.columns(3)
 
 grid = [col.container(height=200) for col in row1 + row2]
 safe_grid = [card.empty() for card in grid]
+
+def black_cats():
+    time.sleep(1)
+    st.title("🐈‍⬛ 🐈‍⬛")
+    st.markdown("🐾 🐾 🐾 🐾")
+
+
+def orange_cats():
+    time.sleep(1)
+    st.title("🐈 🐈")
+    st.markdown("🐾 🐾 🐾 🐾")
+
+def cats:
+    container_a = card_a.container()
+    container_b = card_b.container()
+    container_c = card_c.container()
+    with container_a:
+        black_cats()
+    with container_b:
+        black_cats()
+    with container_c:
+        black_cats()
+
