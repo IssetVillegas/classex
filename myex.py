bc9f9df580b16ba38d68a292ceb702948d2dd20e
IssetVillegas <2022189@student.cct.ie>
2024-12-12 13:07:24 +0000

    Update myex.py

--- myex.py
+++ myex.py
@@ -12,13 +12,13 @@ grid = [col.container(height=200) for col in row1 + row2]
 safe_grid = [card.empty() for card in grid]
 
 def black_cats():
-    time.sleep(1)
+    #time.sleep(1)
     st.title("🐈‍⬛ 🐈‍⬛")
     st.markdown("🐾 🐾 🐾 🐾")
 
 
 def orange_cats():
-    time.sleep(1)
+    #time.sleep(1)
     st.title("🐈 🐈")
     st.markdown("🐾 🐾 🐾 🐾")
 
