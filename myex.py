2a1078fccd9086c6581b366d0f3fb49ea50b8225
IssetVillegas <2022189@student.cct.ie>
2024-12-12 13:02:43 +0000

    Update myex.py

--- myex.py
+++ myex.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("My New App")
 st.write("This is my new Streamlit app Isset Villegas")
+
+row1 = st.columns(3)
+row2 = st.columns(3)
+
+grid = [col.container(height=200) for col in row1 + row2]
+safe_grid = [card.empty() for card in grid]
